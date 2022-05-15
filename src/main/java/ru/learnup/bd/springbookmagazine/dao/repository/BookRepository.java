04ae5343332c4296e43d0b46142b69e4bc976437
kris <chivaskristina@gmail.com>
2022-05-15 10:20:22 +0300

    #25

--- src/main/java/ru/learnup/bd/springbookmagazine/dao/repository/BookRepository.java
+++ src/main/java/ru/learnup/bd/springbookmagazine/dao/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package ru.learnup.bd.springbookmagazine.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.learnup.bd.springbookmagazine.dao.entity.Author;
 import ru.learnup.bd.springbookmagazine.dao.entity.Book;
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @Query("from Book b where b.author = ?1")
     List<Book> findAllByAuthor(Author author);
 
 
